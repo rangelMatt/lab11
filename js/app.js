e00ab268c3e0e4628fed54286386a1c16c767a8f
Matt Rangel <mattrangel@gmail.com>
2022-01-24 13:53:47 -0800

    Scafolding for projects set up

---
+++ js/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// listen to the container for voting
+let myContainer = document.getElementById('conatiner');
+
+// listen to click on the "button" to display results
+let showResults = document.getElementById('show-results');
+
+// js will populate the src - display images
+let imgOne = document.getElementById('img-one');
+let imgTwo = document.getElementById('img-two');
+let imgThee = document.getElementById('img-three');
